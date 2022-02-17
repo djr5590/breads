f13091c163f0bc69def274bb9a8582b4634d68f3
Don Reeves <donjreeves@gmail.com>
2022-02-16 19:41:09 -0500

    create and new routes complete

--- server.js
+++ server.js
@@ -4,25 +4,35 @@ const methodOverride = require('method-override')
 
 // CONFIGURATION
 require('dotenv').config()
+
+// Routes
+const breadsController = require('./controllers/breads_controller.js')
 const PORT = process.env.PORT
 const app = express()
 
-// MIDDLEWARE
+// VIEW ENGINE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use('/breads', breadsController)
+
+// MIDDLEWARE
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
 
-// ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
-})
+// BREAD ROUTES
+
+
+// app.get('/', (req, res) => {
+//   res.send('Welcome to an Awesome App about Breads!')
+// })
 
 // Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+// const breadsController = require('./controllers/breads_controller.js')
+// app.use('/breads', breadsController)
 
 // 404 Page
 app.get('*', (req, res) => {
