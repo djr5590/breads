eed56979eab5035e5063ef1b36a400f9245aec06
Don Reeves <donjreeves@gmail.com>
2022-03-03 13:15:58 -0500

    updated bakers.js

--- models/baker.js
+++ models/baker.js
@@ -1,9 +1,10 @@
 // require mongoose
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose')
 const { Schema } = mongoose
+const Bread = require('./bread')
 
 // schema
-const bakerSchema = new Schema ({
+const bakerSchema = new Schema({
     name: {
         type: String,
         required: true,
@@ -14,6 +15,21 @@ const bakerSchema = new Schema ({
         required: true
     },
     bio: String
+}, {
+    toJSON: {
+        virtuals: true
+    }
+
+})
+
+bakerSchema.virtual('breads', {
+    ref: 'Bread',
+    localField: '_id',
+    foreignField: 'baker'
+})
+bakerSchema.post('findOneAndDelete', async function () {
+    let deleteStatus = await Bread.deleteMany({ baker: this._conditions._id })
+    console.log(deleteStatus)
 })
 
 // model and export
