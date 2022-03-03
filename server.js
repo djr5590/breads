6873f6ac91c98f416fbf34f29fcf7940f6c1d284
Don Reeves <donjreeves@gmail.com>
2022-03-03 14:11:30 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 // DEPENDENCIES
 const express = require('express')
+const app = express()
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
-
-// Routes
-const breadsController = require('./controllers/breads_controller.js')
 const PORT = process.env.PORT
-const app = express()
 
 // VIEW ENGINE
 app.set('views', __dirname + '/views')
@@ -18,28 +15,33 @@ app.engine('jsx', require('express-react-views').createEngine())
 
 // MIDDLEWARE
 app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+// Routes
+app.get('/', (req, res) => {
+  res.redirect('/breads')
+})
+
 // Breads
+const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
 // Bakers
-const bakersController = require('./controllers/bakers_controllers.js')
+const bakersController = require('./controllers/bakers_controller.js')
 app.use('/bakers', bakersController)
 
 // 404 Page
 app.get('*', (req, res) => {
-  res.send('404')
+  res.status(404).render('404')
 })
 
 // Database
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-  // .then(() => console.log('db connected'))
-  .catch(e => console.log(e))
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('db connected'))
+  .catch(e => console.error(e))
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log('nomming at port', PORT);
+  console.log('nomming at port: ${PORT}');
 })
