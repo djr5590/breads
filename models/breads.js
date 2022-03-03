d4ecefeb7303c576c39b8786632acaf7e17a5910
Don Reeves <donjreeves@gmail.com>
2022-03-03 13:27:59 -0500

    added new schema to breads

--- models/breads.js
+++ models/breads.js
@@ -8,9 +8,12 @@ const { Schema } = mongoose
 const breadSchema = new Schema({
   name: { type: String, required: true },
   hasGluten: Boolean,
-  image: { type: String, default: 'http://place-hold.it/500x500.png' },
+  image: { 
+    type: String, 
+    default: 'http://place-hold.it/500x500.png' 
+  },
   baker: {
-    type: Schema.Types.ObjectID,
+    type: Schema.Types.ObjectId,
     ref: "Baker"
   }
 })
@@ -18,6 +21,11 @@ const breadSchema = new Schema({
 // helper methods 
 breadSchema.methods.getBakedBy = function() {
   return `${this.name} was baked with love by ${this.baker.name}, who has been with us since ${this.baker.startDate.getFullYear()}`
+}
+
+breadSchema.statics.findByBaker = async function(bakerName) {
+  let breadArr = await this.find({ baker: bakerName })
+  return breadArr
 }
 
 const Bread = mongoose.model('Bread', breadSchema)
